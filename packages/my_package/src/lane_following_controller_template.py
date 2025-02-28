ac654606f36fa8119901fec6ecc8ab9c52d07d54
saarhin <sarah77.amini@gmail.com>
2025-02-28 15:12:17 -0700

    controller and navigation incomplete

--- packages/my_package/src/lane_following_controller_template.py
+++ packages/my_package/src/lane_following_controller_template.py
@@ -4,42 +4,81 @@
 
 # import required libraries
 
+import rospy
+from duckietown.dtros import DTROS, NodeType
+from duckietown_msgs.msg import WheelsCmdStamped
+import os
+import Float32
+
 class LaneControllerNode(DTROS):
     def __init__(self, node_name):
         super(LaneControllerNode, self).__init__(node_name=node_name, node_type=NodeType.CONTROL)
         # add your code here
         
         # controller type
+        self.control_type = "PID"  # it can be P or PD
+
+        # variables
+        self.image_w = 400
         
         # PID gains 
+        self.proportional_gain = 0.5
+        self.derivative_gain = 0.5
+        self.integral_gain = 0.5
         
         # control variables
+        self.prev_error = 0
+        self.integral = 0
         
         # movement parameters
+        self.speed = 0.5
+        self.error = 0
         
         # distance tracking
         
         # initialize publisher/subscribers
+        vehicle_name = os.environ['VEHICLE_NAME']
+        wheels_topic = f"/{vehicle_name}/wheels_driver_node/wheels_cmd"
+        self.wheel_publisher = rospy.Publisher(wheels_topic, WheelsCmdStamped, queue_size=1)
 
-    def calculate_p_control(self, **kwargs):
-        # add your code here
-        pass
+        lane_topic = f"/{vehicle_name}/lane"
+        lane_sub = rospy.Subscriber("/custom_topic_name", Float32, self.yellow_lane_callback)
 
-    def calculate_pd_control(self, **kwargs):
-        # add your code here
-        pass
+    
 
-    def calculate_pid_control(self, **kwargs):
+    def calculate_p_control(self):
         # add your code here
-        pass
+        return self.proportional_gain * self.error
 
-    def get_control_output(self, **kwargs):
+    def calculate_pd_control(self):
         # add your code here
-        pass
+        derivative = self.error - self.prev_error 
+        return self.proportional_gain * self.error + self.derivative_gain * derivative
+    
+    def calculate_pid_control(self):
+        # add your code here
+        self.integral += self.error
+        derivative = self.error - self.prev_error 
+        return self.proportional_gain * self.error + self.derivative_gain * derivative + self.integral_gain * self.integral
 
-    def publish_cmd(self, **kwargs):
-        # add your code here
-        pass
+    def get_control_output(self):
+        if self.control_type == "P":
+            control = self.calculate_p_control()
+        elif self.control_type == "PD":
+            control = self.calculate_pd_control()
+        elif self.control_type == "PID":
+            control = self.calculate_pid_control()
+        else:
+            rospy.logwarn("Invalid control type!")
+            control = 0.0
+        self.prev_error = self.error
+        return control
+
+    def publish_cmd(self, control):
+        msg = WheelsCmdStamped()
+        msg.vel_left = self.base_speed - control
+        msg.vel_right = self.base_speed + control
+        self.wheel_publisher.publish(msg)
 
     def yellow_lane_callback(self, **kwargs):
         # add your code here
--- packages/my_package/src/navigate_template.py
+++ packages/my_package/src/navigate_template.py
@@ -4,40 +4,126 @@
 
 # import required libraries
 
+import os
+import rospy
+from duckietown.dtros import DTROS, NodeType
+from duckietown_msgs.msg import WheelEncoderStamped
+from duckietown_msgs.msg import WheelsCmdStamped
+from duckietown_msgs.msg import LEDPattern 
+from std_msgs.msg import Header, ColorRGBA 
+import numpy as np
+
 class NavigationControl(DTROS):
     def __init__(self, node_name):
         super(NavigationControl, self).__init__(node_name=node_name, node_type=NodeType.CONTROL)
         # add your code here
+
+        self._vehicle_name = os.environ['VEHICLE_NAME']
+        self._left_encoder_topic = f"/{self._vehicle_name}/left_wheel_encoder_node/tick"
+        self._right_encoder_topic = f"/{self._vehicle_name}/right_wheel_encoder_node/tick"
+        self.wheels_topic = f"/{self._vehicle_name}/wheels_driver_node/wheels_cmd"
+
+        self._ticks_left = 0
+        self._ticks_right = 0
+        self.sub_left = rospy.Subscriber(self._left_encoder_topic, WheelEncoderStamped, self.callback_left)
+        self.sub_right = rospy.Subscriber(self._right_encoder_topic, WheelEncoderStamped, self.callback_right)
+        self.pub = rospy.Publisher(self.wheels_topic, WheelsCmdStamped, queue_size=1)
         
         # publisher for wheel commands
         # NOTE: you can directly publish to wheel chassis using the car_cmd_switch_node topic in this assignment (check documentation)
         # you can also use your exercise 2 code
         
         # robot params
+        self._radius = rospy.get_param(f'/{self._vehicle_name}/kinematics_node/radius', 100)
+        self.DISTANCE_PER_TICK = np.pi*2*self._radius/135
+        self.start_dist = 0
+        self.wheelbase = 0.205
 
         # define other variables as needed
+        self.ROTATION_TARGET = np.pi*self.wheelbase / 4
+        self.ARC_TARGET = np.pi*((self.wheelbase/2)+0.54) / 4
+
+    def callback_left(self, data):
+        self._ticks_left = data.data
+
+    def callback_right(self, data):
+        self._ticks_right = data.data
+
         
     def publish_velocity(self, **kwargs):
         # add your code here
         pass
         
-    def stop(self, **kwargs):
+    def stop(self):
         # add your code here
+        msg = WheelsCmdStamped()
+        msg.vel_left = 0
+        msg.vel_right = 0
+        self.pub.publish(msg)
         pass
         
-    def move_straight(self, **kwargs):
+    def move_straight(self,  speed=0.2, direction=1, distance=1.25, calibrate = 1):
         # add your code here
+        msg = WheelsCmdStamped()
+        msg.vel_left = speed * direction 
+        msg.vel_right = speed * direction * calibrate
+
+        self.start_dist = self.compute_distance_traveled(self._ticks_left)
+
+        while np.abs(self.start_dist - self.compute_distance_traveled(self._ticks_left)) < distance and not rospy.is_shutdown():
+            self.pub.publish(msg)
+
+        msg.vel_left = 0
+        msg.vel_right = 0
+        self.pub.publish(msg)
         pass
         
-    def turn_right(self, **kwargs):
+    def turn_right(self, speed=0.2):
         # add your code here
+        msg = WheelsCmdStamped()
+        msg.vel_left = speed 
+        msg.vel_right = 0
+
+        # Calculate target rotation distance (90 degrees)
+         
+        self.start_dist = self.compute_distance_traveled(self._ticks_left)
+
+
+        while np.abs(self.start_dist - self.compute_distance_traveled(self._ticks_left)) < self.ROTATION_TARGET and not rospy.is_shutdown():
+            self.pub.publish(msg)
+
+
+        # Stop the robot after rotation
+        msg.vel_left = 0
+        msg.vel_right = 0
+        self.pub.publish(msg)
         pass
         
-    def turn_left(self, **kwargs):
+    def turn_left(self, speed=0.2):
         # add your code here
+
+        msg = WheelsCmdStamped()
+        msg.vel_left = -speed 
+        msg.vel_right = 0
+
+        # Calculate target rotation distance (90 degrees)
+         
+        self.start_dist = self.compute_distance_traveled(self._ticks_left)
+
+
+        while np.abs(self.start_dist - self.compute_distance_traveled(self._ticks_left)) < self.ROTATION_TARGET and not rospy.is_shutdown():
+            self.pub.publish(msg)
+
+
+        # Stop the robot after rotation
+        msg.vel_left = 0
+        msg.vel_right = 0
+        self.pub.publish(msg)
         pass
 
     # add other functions as needed
+
+    # TODO: add run function as needed
 
 if __name__ == '__main__':
     node = NavigationControl(node_name='navigation_control_node')
