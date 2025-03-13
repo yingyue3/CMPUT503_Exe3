73d730724c6fc834b05952c582c04935a51d36ac
saarhin <sarah77.amini@gmail.com>
2025-03-13 16:14:42 -0600

    part2and 3

--- packages/my_package/src/lane_detection_template.py
+++ packages/my_package/src/lane_detection_template.py
@@ -39,6 +39,19 @@ class LaneDetectionNode(DTROS):
         self.blue_lower = np.array([110, 80, 80], np.uint8) 
         self.blue_upper = np.array([120, 200, 200], np.uint8) 
 
+        # while bound
+        self.white_lower = np.array([0, 0, 180], np.uint8) 
+        self.white_upper = np.array([180, 40, 255], np.uint8)
+
+        # yellow bound
+        self.yellow_lower = np.array([20, 80, 100], np.uint8) 
+        self.yellow_upper = np.array([40, 255, 255], np.uint8) 
+
+        # Set range for mat
+        self.mat_lower = np.array([20, 40, 40], dtype=np.uint8)   # Lower bound of mat color
+        self.mat_upper = np.array([40, 255, 255], dtype=np.uint8)
+        
+
         # yellow bound
         # self.yellow_lower = np.array([25, 50, 70], np.uint8) 
         # self.yellow_upper = np.array([35, 255, 255], np.uint8) 
@@ -51,11 +64,15 @@ class LaneDetectionNode(DTROS):
         self._bridge = CvBridge()
         self.disorted_image = None
         self.color_detect_image = None
+        self.black_detect_image = None
         self.sub_image = rospy.Subscriber(self._camera_topic, CompressedImage, self.callback_image)
 
         # lane detection publishers
         self._custom_topic = f"/{self._vehicle_name}/custom_node/image/compressed"
         self.pub = rospy.Publisher(self._custom_topic, Image) # queue_size=10
+
+        self._custom_topic_lane = f"/{self._vehicle_name}/custom_node/image/black"
+        self.pub_lane = rospy.Publisher(self._custom_topic_lane, Image) # queue_size=10
 
         # LED
         
@@ -95,6 +112,8 @@ class LaneDetectionNode(DTROS):
         # detect lanes and colors - 1.3
         # publish undistorted image
         self.color_detect_image = self.detect_lane_color(self.disorted_image)
+        self.black_detect_image = self.detect_lane(self.disorted_image)
+
         # control LEDs based on detected colors
 
         # anything else you want to add here
@@ -216,7 +235,81 @@ class LaneDetectionNode(DTROS):
     def detect_lane(self, **kwargs):
         # add your code here
         # potentially useful in question 2.1
-        pass
+
+        height = imageFrame.shape[0]
+        imageFrame = imageFrame[height//3:-height//5, :, :]
+
+
+        imageFrame = cv.GaussianBlur(imageFrame, (5, 5), 0)
+
+        kernel = np.ones((5, 5), "uint8") 
+
+        hsvFrame = cv.cvtColor(imageFrame, cv.COLOR_BGR2HSV)
+
+        white_mask = cv.inRange(hsvFrame, self.white_lower, self.white_upper) 
+        yellow_mask = cv.inRange(hsvFrame, self.yellow_lower, self.yellow_upper) 
+        mat_mask = cv.inRange(hsvFrame, self.mat_lower, self.mat_upper)
+
+        # For white color 
+        white_mask = cv.dilate(white_mask, kernel) 
+        res_white = cv.bitwise_and(imageFrame, imageFrame, 
+                                mask = white_mask) 
+        
+        # For yellow color 
+        yellow_mask = cv.dilate(yellow_mask, kernel) 
+        res_yellow = cv.bitwise_and(imageFrame, imageFrame, 
+                                mask = yellow_mask) 
+
+        # yellow_mask = cv.bitwise_and(yellow_mask, mat_mask)
+        # filtered_white_mask = np.zeros_like(white_mask)
+
+        # for row in range(white_mask.shape[0]):  
+        #     yellow_pixels = np.where(yellow_mask[row, :] > 0)[0]  
+
+        #     if len(yellow_pixels) > 3:  
+        #         yellow_pos = yellow_pixels[-1]  
+
+        #         yellow_mask[row, :yellow_pixels[-2]+1] = 0
+                # yellow_mask[row, yellow_pixels[-1]+1:] = 0
+                
+                # white_pixels = np.where(white_mask[row, yellow_pos:] > 0)[0]  
+
+                # if len(white_pixels) > 0:
+                    
+                #     selected_white_pixels = white_pixels[:1] 
+                #     filtered_white_mask[row, selected_white_pixels] = 100 
+
+        
+        # lane_mask = cv.bitwise_or(yellow_mask, white_mask)
+        lane_mask = np.zeros_like(white_mask)  
+
+        # Set yellow pixels to gray (128)
+        #lane_mask[yellow_mask > 0] = 128  
+
+        # Set white pixels to white (255)
+        lane_mask[white_mask > 0] = 255 
+
+        # Apply the mask to the frame
+        # lane_detection = cv.bitwise_and(imageFrame, imageFrame, mask=lane_mask)
+
+        # contours, hierarchy = cv.findContours(lane_mask, 
+        #                                     cv.RETR_TREE, 
+        #                                     cv.CHAIN_APPROX_SIMPLE) 
+        
+        # for pic, contour in enumerate(contours): 
+        #     area = cv.contourArea(contour) 
+        #     if(area > 300): 
+        #         x, y, w, h = cv.boundingRect(contour) 
+        #         self.color_detect_image = cv.rectangle(imageFrame, (x, y), 
+        #                                 (x + w, y + h), 
+        #                                 (0, 0, 255), 2) 
+                
+        #         cv.putText(self.color_detect_image, "Colour", (x, y), 
+        #                     cv.FONT_HERSHEY_SIMPLEX, 1.0, 
+        #                     (0, 0, 255)) 
+
+
+        return lane_mask
     
 
     # add other functions as needed
@@ -226,7 +319,9 @@ class LaneDetectionNode(DTROS):
             if self.color_detect_image is not None:
                 # rospy.loginfo('publishing image')
                 image_msg = self._bridge.cv2_to_imgmsg(self.color_detect_image, encoding="bgr8")
+                black_msg = self._bridge.cv2_to_imgmsg(self.black_detect_image, encoding="8UC1")
                 self.pub.publish(image_msg)
+                self.pub_lane.publish(black_msg)
             #self.pub.publish(self.raw_image)
         rate.sleep()
 
